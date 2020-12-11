5e00faa1a9ca8d148923e5cc70a7516879082c51
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-10 19:32:11 -0800

    unfinished code, does include Doc's elegant solution

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,32 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # create a frequency variable as a dictionary
+    # count frequency of numbers
+    # search through frequency to find the frequency of 1
+    #return number that has a freq of one
+    frequency = {}
+    for num in arr:
+        #if num is not in frequency yet, then add it with as num
+        if num not in frequency:
+            frequency[num] = 1
+        #if num is already in frequency, increment count by 1
+        else:
+            frequency[num] += 1
+    # print("frequency", frequency)
+    for num, count in frequency.items():
+        if count == 1:
+            return num
 
-    pass
+
+
+    # alt
+    #sort array in place
+    #look check if first 2 match, then check next two until they don't match
+    # if they don't match, return the first number.
+    # if it's the last item, the loop will complete before it covers the last item, so we return the last item
+
+
 
 
 if __name__ == '__main__':
